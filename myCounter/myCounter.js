df24b51af4b2038e31277937d4f7ef46e2b47915
Constantin Nita <nita.constantin880722@yahoo.com>
2022-02-05 23:12:13 +0200

    my counter project

---
+++ myCounter/myCounter.js
@@ -0,0 +1,26 @@
+let count = 0
+var value = document.querySelector('#value')
+var btns = document.querySelectorAll('.btn')
+console.log(btns)
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        var styles = e.currentTarget.classList
+        //console.log(styles)
+        if(styles.contains('decrease')){
+            count--
+        }else if(styles.contains('increase')){
+            count++
+        }else{
+            count = 0
+        }
+
+        if(count > 0){
+            value.style.color = "#00ff00"
+        }else if(count < 0){
+            value.style.color = "#ff0000"
+        }else{
+            value.style.color = "#000000"
+        }
+        value.textContent = count
+    })
+})
